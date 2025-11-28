894656f4351143553b634e6c2b7343a4058228b2
Ricardo <ricarditomontserrat@gmail.com>
2025-11-28 04:27:20 +0100

    cool repl extended
    M  repl/repl.go

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
+	"monkey/parser"
 	"monkey/token"
 )
 
@@ -22,9 +23,19 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			for _, msg := range p.Errors() {
+				fmt.Println(msg)
+			}
+			continue
+		}
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Printf("%+v\n", tok)
+			fmt.Printf("%+v\n", tok)
 		}
+		fmt.Println(program.String())
 	}
 }
